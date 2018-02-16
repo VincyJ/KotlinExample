d830af8fca83ccc9e4fd1d78ad10263e4ee765c7
Vincy <arputhavincyj@gmail.com>
2018-02-16 16:32:32 +0530

    # Deeplink implemented. # Firebase Integrated.

--- app/src/main/java/myportfolio/com/tabswithswipekotlin/MainActivity.kt
+++ app/src/main/java/myportfolio/com/tabswithswipekotlin/MainActivity.kt
@@ -2,6 +2,7 @@ package myportfolio.com.tabswithswipekotlin
 
 import android.app.ActivityOptions
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,8 +13,9 @@ import myportfolio.com.tabswithswipekotlin.swipetabs.SwipeTabsActivity
 
 class MainActivity : AppCompatActivity() {
 
-//    var toolbar: Toolbar? = null
+    //    var toolbar: Toolbar? = null
 //    var btSwipeTabs: Button
+    var data: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,17 @@ class MainActivity : AppCompatActivity() {
         /*val btnSwipeTab = findViewById<Button>(R.id.btn_swipe_tabs)
         val btnSwipeTabs = findViewById(R.id.btn_swipe_tabs) as Button*/
 
+        /*if( intent.getBooleanExtra(Deeplink.IS_DEEP_LINK,false)) {
+            var parameters: Bundle = intent.extras
+            var id: String = parameters.getString("id")
+
+            Log.e("VINCY", intent.extras.toString())
+        }*/
+       /* if (intent != null) {
+            var action: String = intent.action
+            if (intent.data != null)
+                data = intent.data
+        }*/
         btn_swipe_tabs.setOnClickListener {
             startActivity(Intent(this, SwipeTabsActivity::class.java), ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
         }
---
+++ app/src/main/java/myportfolio/com/tabswithswipekotlin/broadcastreceiver/DeeplinkReceiver.java
@@ -0,0 +1,16 @@
+package myportfolio.com.tabswithswipekotlin.broadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by arputha_v on 2/13/2018.
+ */
+
+public class DeeplinkReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
